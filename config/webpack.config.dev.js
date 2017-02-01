1c025cecdce0b6c09422014651b43a7eec363b1b
Chirag Shah <shahchirag1709@gmail.com>
2017-02-01 14:18:30 +0530

    scss loader in webpack config

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -111,7 +111,8 @@ module.exports = {
           /\.(js|jsx)$/,
           /\.css$/,
           /\.json$/,
-          /\.svg$/
+          /\.svg$/,
+          /\.scss$/,
         ],
         loader: 'url',
         query: {
@@ -154,7 +155,12 @@ module.exports = {
         query: {
           name: 'static/media/[name].[hash:8].[ext]'
         }
-      }
+      },
+      // scss loader
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
     ]
   },
   
